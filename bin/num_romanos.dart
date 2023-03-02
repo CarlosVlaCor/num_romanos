5517f3632ae4afad909787ee906d23f7c259fb4a
Carlos E. Valenzuela C <89716956+CarlosVlaCor@users.noreply.github.com>
2023-03-01 18:54:24 -0700

    Funciones para representar decenas y unidades

--- bin/num_romanos.dart
+++ bin/num_romanos.dart
@@ -1,5 +1,4 @@
 import 'package:num_romanos/num_romanos.dart' as num_romanos;
 
 void main(List<String> arguments) {
-  print('Hello world: ${num_romanos.calculate()}!');
 }
--- lib/num_romanos.dart
+++ lib/num_romanos.dart
@@ -1,3 +1,27 @@
-int calculate() {
-  return 6 * 7;
+final Map<int, String> map = {1:"I", 2:"II", 3:"III", 4:"IV", 5:"V", 6:"VI", 7:"VII", 8:"VIII", 9:"IX", 10:"X", 20:"XX",
+            30:"XXX", 40:"XL",50:"L", 60:"LX", 70:"LXX", 80:"LXXX", 90:"XC", 100:"C", 200:"CC", 300:"CCC",
+            400:"CD", 500:"D"};
+
+String regresarRepresentacion(int valor){
+    String valor_str = valor.toString();
+    if(valor_str.length == 1){
+      return representarUnidades(valor);
+    }
+    if(valor_str.length == 2){
+      return representarDecenas(valor_str);
+    }
+    return "";
+}
+String representarDecenas(String valor){
+    List<String> divisionLetras = valor.split("");
+    String decena ="${divisionLetras[0]}0";
+    String representacionDecena = map[int.parse(decena)]!;
+    String representacionUnidad = representarUnidades(int.parse(divisionLetras[1]));
+    return representacionDecena + representacionUnidad;
+}
+
+String representarUnidades(int valor){
+    if(valor == 0) return "";
+    String representacion = map[valor]!;
+    return representacion;
 }
