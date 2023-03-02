872e6c7f8361c0f88762262b053d7538943f868b
Carlos E. Valenzuela C <89716956+CarlosVlaCor@users.noreply.github.com>
2023-03-01 17:12:38 -0700

    Proyect created

---
+++ bin/num_romanos.dart
@@ -0,0 +1,5 @@
+import 'package:num_romanos/num_romanos.dart' as num_romanos;
+
+void main(List<String> arguments) {
+  print('Hello world: ${num_romanos.calculate()}!');
+}
---
+++ test/num_romanos_test.dart
@@ -0,0 +1,8 @@
+import 'package:num_romanos/num_romanos.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
